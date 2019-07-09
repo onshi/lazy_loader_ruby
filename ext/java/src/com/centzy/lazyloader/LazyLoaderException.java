0065dc4c435aaad22da7086b5d0f24d6b9889949
Jan Makara <jan.makara@hotmail.com>
2019-07-09 20:40:31 +0200

    Revert "Deprecation notice"
    This reverts commit a0a101fb31f38b7785e590dffe6edf95dae9e6f9.

---
+++ ext/java/src/com/centzy/lazyloader/LazyLoaderException.java
@@ -0,0 +1,11 @@
+package com.centzy.lazyloader;
+
+/**
+ * @author Peter Edge (peter.edge@gmail.com).
+ */
+public class LazyLoaderException extends Exception {
+
+  LazyLoaderException(Throwable cause) {
+    super(cause);
+  }
+}
